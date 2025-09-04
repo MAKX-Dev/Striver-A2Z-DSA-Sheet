2f8ee8552d3a7e916ea05499e1acc498f5019e99
MAKX-Dev <manthankhotele7@gmail.com>
2025-09-04 21:39:49 +0530

    Add initial C++ programs

---
+++ If_else_statements.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cin>>marks;
+    if(marks>=90){
+        cout<<"Grade A"<<endl;
+    }
+    if(marks>=90){
+        cout<<"Grade B"<<endl;
+
+    }
+    if(marks>=50){
+        cout<<"Grade C"<<endl;
+    }
+    else{
+        cout<<"Grade F"<<endl;
+    }
+
+
+}
---
+++ Switch_statement.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int day;
+    cin>>day;
+    switch(day){
+        case1:cout<<"Monday"<<endl;
+        break;
+        case2:cout<<"Tuesday"<<endl;
+        break;
+        case3:cout<<"Wednusday"<<endl;
+        break;
+        case4:cout<<"Thursday"<<endl;
+        break;
+        case5:cout<<"Friday"<<endl;
+        break;
+        case6:cout<<"Saturay"<<endl;
+        break;
+        case7:cout<<"Sunday"<<endl;
+        break;
+        default:cout<<"Invalid"<<endl;
+        break;
+    }
+    return 0;
+}
