33e624fac3d323f78cdf595d4534146aeab08863
MAKX-Dev <manthankhotele7@gmail.com>
2025-10-28 21:56:00 +0530

    ✨ feat bug fix: 14_Find_square_root_of_a_number

--- 04 Binary Search/14_Find_square_root_of_a_number.cpp
+++ 04 Binary Search/14_Find_square_root_of_a_number.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 int floorSqrt(int n){
+    if(n==0) return 0;
     int low=1;
     int high=n;
     int ans=-1;
     while(low<=high){
-        int mid=(high+low)/2;
+        long long mid=low+(high-low)/2;
         if(mid*mid<=n){
             ans=mid;
             low=mid+1;
