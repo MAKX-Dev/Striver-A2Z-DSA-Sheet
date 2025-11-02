7454673014e8fd6d4808c13d21e29caa326c0830
MAKX-Dev <manthankhotele7@gmail.com>
2025-11-02 17:15:19 +0530

    ✨ +2 Recursion

---
+++ 07 Recursion/18_Word_search.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool search(vector<vector<char>>& board,string& word,int r,int c,int index){
+        if (index==word.length()){
+            return true;
+        }
+        if (r<0 || c<0 || r>=board.size() || c>=board[0].size()){
+            return false;
+        }
+        if (board[r][c]!=word[index]){
+            return false;
+        }
+        char temp=board[r][c];
+        board[r][c]='#';
+        bool found=search(board,word,r+1,c,index+1) ||
+                     search(board,word,r-1,c,index+1) || 
+                     search(board,word,r,c+1,index+1) || 
+                     search(board,word,r,c-1,index+1); 
+        board[r][c]=temp;
+        return found;
+    }
+bool exist(vector<vector<char>>& board,string word){
+        int rows=board.size();
+        int cols=board[0].size();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(board[i][j]==word[0]){
+                    if(search(board,word,i,j,0)){
+                        return true; 
+                    }
+                }
+            }
+        }
+        return false;
+    }
+int main(){
+    vector<vector<char>> board={{'A', 'B', 'C', 'E'},{'S', 'F', 'C', 'S'},{'A', 'D', 'E', 'E'}};
+    string word="ABCCED";
+    bool result=exist(board, word);
+    cout<<(result ? "Found" : "Not Found")<<endl;
+    return 0;
+}
