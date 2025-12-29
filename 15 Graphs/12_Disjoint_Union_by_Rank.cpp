a25153fcad8255cb583257f4309ac2f4479c5e76
MAKX Dev <manthankhotele7@gmail.com>
2025-12-29 19:42:53 +0530

    ✨ +1 Graphs

---
+++ 15 Graphs/12_Disjoint_Union_by_Rank.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+class DisjointSet{
+  vector<int> rank, parent;
+  public:
+  DisjointSet(int n ) {
+    rank.resize(n+1, 0);
+    parent.resize(n+1);
+    for(int i = 0; i <= n; i++) {
+      parent[i] = i;
+    }
+  }
+
+findUparent(int node) {
+  if(node == parent[node]) return node;
+  return parent[node] = findUparent(parent[node]);
+}
+
+void UnionByRank(int u, int v) {
+  int ulp_u = findUparent(u);
+  int ulp_v = findUparent(v);
+  if(ulp_v == ulp_u) return;
+  if(rank[ulp_u] < rank[ulp_v]) {
+    parent[ulp_u] = ulp_v;
+  }
+  else if(rank[ulp_v] < rank[ulp_u]) {
+    parent[ulp_v] = ulp_u;
+  }
+  else {
+    parent[ulp_v] = ulp_u;
+    rank[ulp_u]++;
+  }
+}
+};
+
+int main() {
+  DisjointSet ds(7);
+  ds.UnionByRank(1, 2);
+  ds.UnionByRank(2, 3);
+  ds.UnionByRank(4, 5);
+  ds.UnionByRank(6, 7);
+  if(ds.findUparent(3) == ds.findUparent(7)) {
+    cout << "same" << endl;
+  }
+  else {
+    cout << "not same" << endl;
+  }
+  ds.UnionByRank(3, 7);
+  return 0;
+}
