9f4572475e265b89b5da88d148f33d77ff3e937f
MAKX Dev <manthankhotele7@gmail.com>
2026-01-06 21:57:56 +0530

    ✨ +1 Dynamic Programming

---
+++ 16 Dynamic Programming/11_Minimum_path_sum_in_triangular_grid.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minTriangleSum(vector<vector<int>>& triangle) {
+  int n = triangle.size();
+  vector<vector<int>> dp(n, vector<int> (n, 0));
+  for(int j = 0; j < n; j++) {
+    dp[n-1][j] = triangle[n-1][j];
+  }
+  for(int i = n-2; i >= 0; i--) {
+    for(int j = 0; j <= i; j++) {
+      dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+    }
+  }
+  return dp[0][0];
+}
+
+int main() {
+  vector<vector<int>> triangle = {{1}, {4, 7}, {4,10, 50}, {-50, 5, 6, -100}};
+  int result = minTriangleSum(triangle);
+  cout << result << endl;
+  return 0;
+}
